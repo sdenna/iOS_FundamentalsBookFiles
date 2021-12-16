86f44faa04135926f651f4c66a3e0d1c2aeb93e4
Shannon Denna <sdenna@d211.org>
2021-12-15 19:14:04 -0600

    Completed collections and dictionaries

--- 2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -8,22 +8,38 @@ var days = ["January": 31, "February": 28, "March": 31]
 print(days)
 
 //:  Using subscripting syntax to add April to the collection with a value of 30. Print the dictionary.
-//days["April"] = 30      // should add April - why doesn't it?
+days["April"] = 30
 print("hi")
 print(days)
 
 //:  It's a leap year! Update the number of days in February to 29 using the `updateValue(_:, forKey:)` method. Print the dictionary.
-//let oldNum = days.updateValue(29, forKey: "February")
-//print(oldNum)
-//print(days)
+let oldValue = days.updateValue(29, forKey: "February")
+print(oldValue!)    // putting the ! after the variable removes the optional part
+                    // in front of it saying that we KNOW there is a value there
+print(days)
+let missingValue = days["December"]
+print(missingValue) // bc there isn't an entry with December, there isn't a matching
+                    // value, so nil is returned
 
 //:  Use if-let syntax to retrieve the number of days under "January". If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
+// an if-let is a safe way to do an if statement.  If the variable has a value,
+// then the code will execute.  If it doesn't, then it wont'T
 
+if let num = days["January"] {
+    print("January has \(num) days")
+}
 
 //:  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors". Print the resulting dictionary.
+var shapesArray: [String] = ["Circle", "Square", "Triangle"]
+var colorsArray: [String] = ["Red", "Blue", "Green", "Purple"]
 
+var myDictionary = ["Shapes": shapesArray, "Colors": colorsArray]
+print(myDictionary)
 
 //:  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the force unwrap operator to unwrap what is returned from the dictionary before you can access an element of the array.
+if let myArr = myDictionary["Colors"] {
+    print(myArr[myArr.count-1])
+}
 
 
 /*:
